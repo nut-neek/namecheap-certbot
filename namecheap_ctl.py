f80e45a01d833a16772cb34fbb297bffe6864ae4
roman <roman.sarakvasa@persequor.com>
2024-10-06 19:18:42 +0200

    Add cleanup hook

--- namecheap_ctl.py
+++ namecheap_ctl.py
@@ -7,13 +7,12 @@ from _namecheap_client import Records
 from argparse import ArgumentParser
 from dotenv import load_dotenv
 
-
 parser = ArgumentParser()
+parser.add_argument("mode", choices=['auth', 'cleanup'], help="Specifies a mode.")
 parser.add_argument("-e", "--env", type=str,
                     help="Name of the environment. Defines which env-file to read. If not specified `.env` file is "
                          "used. E.g. if --env=PROD, `.PROD.env` config file will be use.")
 args = parser.parse_args()
-
 
 CERTBOT_DOMAIN = os.getenv('CERTBOT_DOMAIN')
 NEW_CHALLENGE = os.getenv('CERTBOT_VALIDATION')
@@ -21,7 +20,7 @@ NEW_CHALLENGE = os.getenv('CERTBOT_VALIDATION')
 load_dotenv(f'.{args.env}.env')
 
 
-def set_new_challenge():
+def authenticate():
     records = Records(CERTBOT_DOMAIN)
     records.fetch_records()
     records.set_challenge(NEW_CHALLENGE)
@@ -30,5 +29,15 @@ def set_new_challenge():
     time.sleep(60)
 
 
+def cleanup():
+    records = Records(CERTBOT_DOMAIN)
+    records.remove_challenge()
+    records.post_records()
+    records.fetch_records()
+
+
 if __name__ == "__main__":
-    set_new_challenge()
+    if args.mode == 'auth':
+        authenticate()
+    else:
+        cleanup()
